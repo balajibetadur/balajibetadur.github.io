815897930c40e9b8cae23cff6e969d83dccf652d
balajibetadur <balajibetadur@gmail.com>
2022-08-20 00:15:00 +0530

    Updates

---
+++ precache-manifest.931042cf16025e903cdc0ace46f7b83d.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "43d8e486341c2a32895d",
+    "url": "/static/css/main.d97db14f.chunk.css"
+  },
+  {
+    "revision": "43d8e486341c2a32895d",
+    "url": "/static/js/main.43d8e486.chunk.js"
+  },
+  {
+    "revision": "256abf6866fa4d3d57d8",
+    "url": "/static/js/1.256abf68.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "948dd3c46d26b262fe4c6a1dee4cf3bd",
+    "url": "/static/media/flask.948dd3c4.png"
+  },
+  {
+    "revision": "964a9cebc6ba6fbfd32c02ece5f988e8",
+    "url": "/static/media/sklearn.964a9ceb.png"
+  },
+  {
+    "revision": "aba3531c54d2a85939da13dda7b93350",
+    "url": "/static/media/keras.aba3531c.png"
+  },
+  {
+    "revision": "191747e07c9d80f8e3d1094e3647cae2",
+    "url": "/static/media/react.191747e0.webp"
+  },
+  {
+    "revision": "10ef412d513b343b63ffbc0062ce63f5",
+    "url": "/static/media/firebase.10ef412d.png"
+  },
+  {
+    "revision": "4d4d536c76d42adcb3616f3c295a997f",
+    "url": "/static/media/redux.4d4d536c.png"
+  },
+  {
+    "revision": "65014027656c6317e2b88c63f5156cdc",
+    "url": "/static/media/aws.65014027.png"
+  },
+  {
+    "revision": "ba5b0cd091207fd4d8cd46c0f13debfb",
+    "url": "/static/media/fastai.ba5b0cd0.png"
+  },
+  {
+    "revision": "66408dc25887123d6aa71f180aa4c497",
+    "url": "/static/media/opencv.66408dc2.png"
+  },
+  {
+    "revision": "1c6a3bfc5ed14c7145f5b815c5a986aa",
+    "url": "/static/media/selenium.1c6a3bfc.png"
+  },
+  {
+    "revision": "97084ee813754f4fd50f8d06d7d23f11",
+    "url": "/static/media/sql.97084ee8.png"
+  },
+  {
+    "revision": "00af34111cb9e5703289dda8f3329df9",
+    "url": "/static/media/gcp.00af3411.png"
+  },
+  {
+    "revision": "788bed340525d695037c1855bb2f106b",
+    "url": "/static/media/bootstrap.788bed34.png"
+  },
+  {
+    "revision": "73d550c4418c231c1f13916e88b9ead2",
+    "url": "/static/media/dp.73d550c4.png"
+  },
+  {
+    "revision": "5032d5af74849a30dd2af4e056476428",
+    "url": "/static/media/About.5032d5af.svg"
+  },
+  {
+    "revision": "23ade08ab25920a4b0b9048dd5fc436b",
+    "url": "/static/media/DataScience.23ade08a.svg"
+  },
+  {
+    "revision": "27c5d9a65c42f37daada213489a52fb7",
+    "url": "/static/media/Frontend.27c5d9a6.svg"
+  },
+  {
+    "revision": "ea92d0189fb25e2c158017883ca21980",
+    "url": "/static/media/Cloud.ea92d018.svg"
+  },
+  {
+    "revision": "3e3a8fac78054c8d373ce5e506c91d0a",
+    "url": "/static/media/Experience.3e3a8fac.svg"
+  },
+  {
+    "revision": "e8e8d331eb52763c365d79ab37a32f4e",
+    "url": "/static/media/klsgit.e8e8d331.png"
+  },
+  {
+    "revision": "6d57c1f77c528d518b80b35f88aa2a85",
+    "url": "/static/media/Education.6d57c1f7.svg"
+  },
+  {
+    "revision": "05a8dec30e4febd9ec416aeb2f8a5ff2",
+    "url": "/static/media/Projects.05a8dec3.svg"
+  },
+  {
+    "revision": "af66a47ab9d6c1475432fbdd6a15ae1f",
+    "url": "/static/media/pyttsx3.af66a47a.png"
+  },
+  {
+    "revision": "9ffc1b39f6c3159e36fcc8c7801391fd",
+    "url": "/static/media/Achievements.9ffc1b39.svg"
+  },
+  {
+    "revision": "dcef46d1b4eebe4d9d4f9c32aeab9064",
+    "url": "/static/media/contact.dcef46d1.svg"
+  },
+  {
+    "revision": "923128492861dc1d458e3380a0d0dfdd",
+    "url": "/index.html"
+  }
+];
