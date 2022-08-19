f3f5dccfe5a0a60d88c641c6a61868e755e731d7
balajibetadur <balajibetadur@gmail.com>
2022-08-20 01:34:46 +0530

    cosmetic changes

--- src/Data/About.js
+++ src/Data/About.js
@@ -3,8 +3,8 @@ import AboutImage from "../Assets/About.svg";
 const AboutUser = {
 	description: [
 		"⚡I am a computer science graduate student (2021) . I write Python, Javascript and C++ sometimes. Most of the time I am either building/creating something or spending time with my family and friends, and the rest of the time I am usually sleeping.",
-		"⚡Looking for SDE full-time roles related to Software Engineering, Data Science, Machine Learning or Fronend development. I am interested in leadership opportunities where my contribution can make a difference.",
-		"⚡My hobbies include painting, photography and travelling. I love playing chess, exploring new places and interested in AI.",
+		"⚡Looking for SDE full-time roles related to Software Engineering, Data Science, Machine Learning or Frontend development. I am interested in leadership opportunities where my contribution can make a difference.",
+		"⚡My hobbies include painting, photography and traveling. I love playing chess, exploring new places and interested in AI.",
 	],
 	image: AboutImage,
 };
