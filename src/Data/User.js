8f838f1088bb93dcceff7f6ec2f0354d3c3a1c39
balajibetadur <balajibetadur@gmail.com>
2022-09-28 03:36:51 +0530

    resume updated

--- src/Data/User.js
+++ src/Data/User.js
@@ -6,7 +6,7 @@ const User = {
     gmail: 'mailto:balajibetadur@gmail.com',
     mobile: '+91 8892011406',
     contactPrompt: 'I am available on almost every social media. You can message me; I will reply within 24 hours. I would love to work and help regarding Machine Learning, Data Science, Computer Vision, Natural Language Processing, and Frontend development.',
-    resume: 'https://drive.google.com/file/d/1PJb2F4bSFWoIP08lieoEwEGakZ-iDV6w/view?usp=sharing'
+    resume: 'https://drive.google.com/file/d/1vkB6J0O2gu9lWbPgZbUCxYFu23tJr54d/view?usp=sharing'
 }
 
 
